947db8ab8b33665ea244dba01bfc2cf3df79c221
sg7893 <silina_george@hotmail.com>
2019-08-10 09:41:22 -0400

    navbar

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+  ],
+  imports: [
+    NgbModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
