22ad8404853cdc8f831dd66095d286e54f5cf899
sg7893 <silina_george@hotmail.com>
2019-08-10 18:43:07 -0400

    signup page UI finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css', '../../node_modules/hamburgers/_sass/hamburgers/hamburgers.scss'],
 })
 export class AppComponent {
 
