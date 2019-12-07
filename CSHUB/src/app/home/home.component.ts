4c77ff9e17c493f46d35e790397bec0eb7fb1e64
Silina George <19698115+SilinaGeorge@users.noreply.github.com>
2019-12-07 14:38:45 -0500

    finished landingpage/frontpage

--- CSHUB/src/app/home/home.component.ts
+++ CSHUB/src/app/home/home.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ParallaxScrollModule} from 'ng2-parallaxscroll';
 import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource, NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
-
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-  providers: [NgbCarouselConfig]
+  styleUrls: ['./home.component.css', 
+  '../../assets/landingpage/css/noscript.css',
+   '../../assets/landingpage/css/main.css',
+   '../../assets/landingpage/sass/noscript.scss',
+   '../../assets/landingpage/sass/main.scss'
+  ],
+  providers: [NgbCarouselConfig],
+
 })
+
+
+
 export class HomeComponent implements OnInit {
 
 
