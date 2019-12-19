c515e845996003de135592be8e3781622ca631da
Silina George <19698115+SilinaGeorge@users.noreply.github.com>
2019-12-18 22:01:05 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -50,7 +50,7 @@ app.use(express.static(dist));
 
 // Create server to listen for connections
 //const server = http.createServer(app);
-http.createServer({}, app).listen(port, () => {
+http.createServer({}, app).listen(process.env.PORT || port, () => {
   console.log('Listening...')
 })
 //server.listen(port, () => console.log("listening on port " + port));
