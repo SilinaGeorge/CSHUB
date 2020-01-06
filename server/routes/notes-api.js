df76d301245b1d28ad102227af6571b4d2c9e7ee
Silina George <19698115+SilinaGeorge@users.noreply.github.com>
2020-01-06 13:36:16 -0500

    content optional in notes

--- server/routes/notes-api.js
+++ server/routes/notes-api.js
@@ -41,7 +41,7 @@ const isAuthorizedBody = function (req, res, next) {
 router.post("/:id", isAuthenticated, isAuthorized, [
   check('name', 'name is invalid').trim().escape().isLength({ max: 50 }).not().isEmpty(),
   check('description', 'description is invalid').trim().escape().isLength({ max: 210 }),
-  check('content', 'invalid content').isLength({ max: 5000000 }).optional(),//.trim().escape(),
+  check('content', 'invalid content').isLength({ max: 5000000 }).optional({ default: '' }),//.trim().escape(),
   check('topic', 'invalid topic').trim().escape().isLength({ max: 50 }).not().isEmpty().isIn(validTopics),
   param('id', 'Invalid ID').isAlphanumeric().trim().escape().not().isEmpty()
 ], (req, res, next) => {
@@ -195,7 +195,7 @@ router.patch("/:id", isAuthenticated, isAuthorizedBody, [
   check('userId', 'userId is invalid').isAlphanumeric().trim().escape().not().isEmpty(),
   check('name', 'name is invalid').trim().escape().isLength({ max: 50 }).not().isEmpty(),
   check('description', 'description is invalid').trim().escape().isLength({ max: 210 }),
-  check('content', 'content too big').optional().isLength({ max: 5000000 })//.trim().escape(),
+  check('content', 'content too big').optional({ default: '' }).isLength({ max: 5000000 })//.trim().escape(),
 ], (req, res, next) => {
 
 
