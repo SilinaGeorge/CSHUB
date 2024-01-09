5fa91928385c3b8c54c2dc0f814c96b492c55f1c
Silina George <19698115+SilinaGeorge@users.noreply.github.com>
2024-01-09 17:22:29 -0500

    Update auth.js

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,14 +1,14 @@
 module.exports= {
     facebookAuth: {
-        clientSecret: '322220606d24f17e5fd4cf48549708e1',
-        clientID: '2426554690949520',
-        callbackURL: 'https://localhost:4200/auth/facebook/callback' 
+        clientSecret: 'secret',
+        clientID: 'id',
+        callbackURL: 'http://localhost:8000/auth/facebook/callback' 
     },
 
     googleAuth: {
-        clientSecret: 'C6G2KCzmKn_t-zDM8l1hSHp6',
-        clientID: '921142445428-fteph28ea4vjvq6k03v3joiekb0c8i50.apps.googleusercontent.com',
-        callbackURL: 'https://localhost:4200/auth/google/callback' 
+        clientSecret: 'secret',
+        clientID: 'id',
+        callbackURL: 'http://localhost:8000/auth/google/callback' 
     }
 
 }
